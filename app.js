4ec511c1ef8e116eacf6117224c65d596e266de3
MuhammadNurulloh <dodgechalanger9@gmail.com>
2024-10-06 17:40:50 +0300

    4.7 dars

---
+++ app.js
@@ -0,0 +1,24 @@
+// UYGA VAZIFA
+
+// 26
+// function num(a, b, c) {
+//   if (a == 4) {
+//     console.log("Folse");
+//   } else {
+//     console.log("True");
+//   }
+//   if (b == 9) {
+//     console.log("Folse");
+//   } else {
+//     console.log("True");
+//   }
+//   if (c == 7) {
+//     console.log("True");
+//   } else {
+//     console.log("Folse");
+//   }
+//   return a, b, c;
+// }
+// console.log(num(4, 9, 7));
+
+// 27
